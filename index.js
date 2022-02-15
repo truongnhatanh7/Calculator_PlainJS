777aebde960e348975ff2f96778d15809bcd27dd
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-02-15 20:57:56 +0700

    Update logic

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const $$ = document.querySelectorAll.bind(document)
 
 const operators = $$(".controller__operator-btn")
 const digits = $$(".controller__digit-btn")
-const decimal = $(".decimal")
 const reset = $("#reset")
 const result = $("#result")
+const display = $(".display__value")
 
 let currentResult = 0
 let currentNumber = ""
-let firstNumber = 0
 let currentOperator = ""
 let dotCounter = 0
+
+displayOnScreen(0)
 
 for (let number of digits) {
     number.onclick = function() {
@@ -21,33 +22,51 @@ for (let number of digits) {
             dotCounter++
         }
         currentNumber += number.value
+        displayOnScreen(currentNumber)
     }
 }
 
 for (let operator of operators) {
     operator.onclick = function() {
-        let currentNumberVal = new Number(currentNumber)
-        if (operator.value === "+") {
-            currentResult += currentNumberVal
-
-        } else if (operator.value === "-") {
-            currentResult -= currentNumberVal
-
-        } else if (operator.value === "x") {
-            currentResult *= currentNumberVal
-        } else {
-            if (currentResult === 0) {
-                currentResult = currentNumberVal
-            } else {
-                currentResult /= currentNumberVal
-            }
-        }
+        currentResult += new Number(currentNumber)
         currentNumber = ""
         currentOperator = operator.value
     }
 }
 
 result.onclick = function() {
-    console.log(currentResult)
+    let currentNumberVal = new Number(currentNumber)
+    currentNumber = ""
+    if (currentOperator === "+") {
+        currentResult += currentNumberVal
+
+    } else if (currentOperator === "-") {
+        currentResult -= currentNumberVal
+
+    } else if (currentOperator === "x") {
+        if (currentResult === 0) {
+            currentResult = currentNumberVal
+        } else {
+            currentResult *= currentNumberVal
+        }
+    } else {
+        if (currentResult === 0) {
+            currentResult = currentNumberVal
+        } else {
+            currentResult /= currentNumberVal
+        }
+    }
+    displayOnScreen(currentResult)
+
+}
+
+reset.onclick = function() {
+    currentResult = 0
+    displayOnScreen(0)
+
+}
+
+function displayOnScreen(content) {
+    display.innerHTML = content
 }
 
