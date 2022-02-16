94fe5c06e35ae1a1641aad7ecac3db1cffa3086f
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-02-16 13:52:21 +0700

    Update logic for calculating numbers

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const digits = $$(".controller__digit-btn")
 const reset = $("#reset")
 const result = $("#result")
 const display = $(".display__value")
+const dot = $("#dot")
 
 let currentResult = 0
 let currentNumber = ""
@@ -16,52 +17,71 @@ displayOnScreen(0)
 
 for (let number of digits) {
     number.onclick = function() {
-        if (number.value === "." && dotCounter === 1) {
-            return
-        } else {
-            dotCounter++
-        }
         currentNumber += number.value
+        console.log(currentNumber)
         displayOnScreen(currentNumber)
+    }
+}
+
+dot.onclick = function() {
+    if (dotCounter === 0) {
+        currentNumber += "."
+        console.log(currentNumber)
+        displayOnScreen(currentNumber)
+        dotCounter = 1
     }
 }
 
 for (let operator of operators) {
     operator.onclick = function() {
-        currentResult += new Number(currentNumber)
-        currentNumber = ""
+        dotCounter = 0
+        let currentNumberVal = new Number(currentNumber)
         currentOperator = operator.value
+        if (currentResult !== 0) { // In calculation (Ex: 3+2+7)
+            displayOnScreen(currentResult)
+            calculateResult(currentNumberVal)
+            displayOnScreen(currentResult)
+        } else { // New calculation
+            currentResult = new Number(currentNumber)
+            currentOperator = operator.value
+        }
+        currentNumber = ""
     }
 }
 
 result.onclick = function() {
     let currentNumberVal = new Number(currentNumber)
     currentNumber = ""
-    if (currentOperator === "+") {
-        currentResult += currentNumberVal
-
-    } else if (currentOperator === "-") {
-        currentResult -= currentNumberVal
-
-    } else if (currentOperator === "x") {
-        if (currentResult === 0) {
-            currentResult = currentNumberVal
-        } else {
-            currentResult *= currentNumberVal
-        }
-    } else {
-        if (currentResult === 0) {
-            currentResult = currentNumberVal
-        } else {
-            currentResult /= currentNumberVal
-        }
-    }
+    calculateResult(currentNumberVal)
     displayOnScreen(currentResult)
 
 }
 
+function calculateResult(number) {
+    if (currentOperator === "+") {
+        currentResult += number
+
+    } else if (currentOperator === "-") {
+        currentResult -= number
+
+    } else if (currentOperator === "x") {
+        if (currentResult === 0) {
+            currentResult = number
+        } else {
+            currentResult *= number
+        }
+    } else {
+        if (currentResult === 0) {
+            currentResult = number
+        } else {
+            currentResult /= number
+        }
+    }
+}
+
 reset.onclick = function() {
     currentResult = 0
+    currentOperator = ""
     displayOnScreen(0)
 
 }
@@ -69,4 +89,3 @@ reset.onclick = function() {
 function displayOnScreen(content) {
     display.innerHTML = content
 }
-
